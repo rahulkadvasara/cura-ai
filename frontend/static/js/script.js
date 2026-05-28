91c86074c327d461308601bbed30181b5273ad10
Rahul Kumar <kadvasararahul@gmail.com>
2026-05-28 20:51:29 +0530

    .

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = "http://127.0.0.1:5000";
+// const BACKEND_URL = "http://127.0.0.1:5000";
+const BACKEND_URL = "https://cura-ai-8erp.onrender.com/";
 
 /* ================= AUTH ================= */
 
